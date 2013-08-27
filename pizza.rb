a672f806a7bcde1410129db552e85ed19d2da585
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-27 14:51:24 -0500

    made topping tests pass

--- pizza.rb
+++ pizza.rb
@@ -1,9 +1,23 @@
 class Pizza
-  attr_reader :name, :time_baked, :description
+  attr_reader :name, :time_baked, :description, :toppings
   def initialize(name, time_baked=0)
 
     @time_baked = time_baked
     @name = name
     @description = "This is a description of the pizza."
+    @toppings = []
+  end
+
+  def required_bake_time
   end
 end
+
+
+class Topping
+  attr_reader :name, :required_bake_time
+  def initialize(name, required_bake_time)
+    @name = name
+    @required_bake_time = required_bake_time
+  end
+end
+
